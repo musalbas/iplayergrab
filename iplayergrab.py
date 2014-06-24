5d7b75d007d8793ced5d3665e140fec3124b0826
Mustafa <mus@musalbas.com>
2014-06-24 14:26:05 +0100

    Restructure, and add pagination

--- iplayergrab.py
+++ iplayergrab.py
@@ -2,74 +2,98 @@ import os
 import time
 import urllib2
 
-INTERVAL = None
 
-CATEGORIES = [
-    'arts',
-    'cbbc',
-    'cbeebies',
-    'comedy',
-    'documentaries',
-    'drama-and-soaps',
-    'entertainment',
-    'films',
-    'food',
-    'history',
-    'music',
-    'news',
-    'science-and-nature',
-    'sport',
-    'audio-described',
-    'signed',
-    'northern-ireland',
-    'scotland',
-    'wales',
-]
+class IplayerGrab:
 
-CHANNELS = [
-    'bbcone',
-    'bbctwo',
-    'bbcthree',
-    'bbcfour',
-    'cbbc',
-    'cbeebies',
-    'bbcnews',
-    'bbcparliament',
-    'bbcalba',
-]
+    categories = [
+        'arts',
+        'cbbc',
+        'cbeebies',
+        'comedy',
+        'documentaries',
+        'drama-and-soaps',
+        'entertainment',
+        'films',
+        'food',
+        'history',
+        'music',
+        'news',
+        'science-and-nature',
+        'sport',
+        'audio-described',
+        'signed',
+        'northern-ireland',
+        'scotland',
+        'wales',
+    ]
 
-BASEURL = "http://bbc.co.uk/"
+    channels = [
+        'bbcone',
+        'bbctwo',
+        'bbcthree',
+        'bbcfour',
+        'cbbc',
+        'cbeebies',
+        'bbcnews',
+        'bbcparliament',
+        'bbcalba',
+    ]
+
+    baseurl = 'http://bbc.co.uk/'
+
+    def __init__(self, dirname):
+        self._urls = self._generate_urls()
+        self._dirname = str(dirname)
+
+        os.mkdir(self._dirname)
+
+    def _generate_urls(self):
+        urls = []
+
+        for channel in self.channels:
+            urls.append(channel)
+
+        for category in self.categories:
+            urls.append('iplayer/categories/' + category)
+            urls.append('iplayer/categories/' + category + '/all?sort=dateavailable&page={PAGE}')
+            urls.append('iplayer/categories/' + category + '/all?sort=atoz&page={PAGE}')
+
+        urls.append('iplayer/group/most-popular')
+        urls.append('iplayer')
+
+        return urls
+
+    def _save_page(self, url):
+        page = urllib2.urlopen(self.baseurl + url).read()
+
+        if '<p class="error-message">' in page:
+            return False
+
+        filename = url.replace('/', '-') + '.html'
+        filestream = open(self._dirname + '/' + filename, 'w')
+        filestream.write(page)
+        filestream.close()
+
+        return True
+
+    def run(self, printprogress=False):
+        for url in self._urls:
+            if printprogress:
+                print url
+
+            if '{PAGE}' in url:
+                pagecounter = 1
+                while True:
+                    pageurl = url.replace('{PAGE}', str(pagecounter))
+
+                    if not self._save_page(pageurl):
+                        break
+
+                    pagecounter += 1
+                    print pageurl
+
+            else:
+                self._save_page(url)
 
 if __name__ == '__main__':
-    urls = []
-
-    for channel in CHANNELS:
-        urls.append(channel)
-
-    for category in CATEGORIES:
-        urls.append("iplayer/categories/" + category)
-        urls.append("iplayer/categories/" + category + "/all?sort=dateavailable")
-
-    urls.append("iplayer/group/most-popular")
-    urls.append("iplayer")
-
-    while True:
-        dirname = str(int(time.time()))
-        os.mkdir(dirname)
-
-        print dirname
-        print "---"
-
-        for url in urls:
-            print url
-            filename = url.replace("/", "-") + ".html"
-            filestream = open(dirname + "/" + filename, 'w')
-            filestream.write(urllib2.urlopen(BASEURL + url).read())
-            filestream.close()
-
-        if INTERVAL is not None:
-            print
-            time.sleep(INTERVAL)
-        else:
-            break
-
+    IplayerGrab(dirname=int(time.time())).run(printprogress=True)
